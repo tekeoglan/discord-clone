6c48d9e98caf1522564663732d0005426494ebd0
Hasan A. Tekeoğlu <55619655+tekeoglan@users.noreply.github.com>
2023-06-30 12:07:28 +0300

    feat(): add user repository

---
+++ server/repository/user_repository.go
@@ -0,0 +1,44 @@
+package repository
+
+import (
+	"context"
+	"github/tekeoglan/discord-clone/model"
+	"github/tekeoglan/discord-clone/mongo"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type userRepository struct {
+	database   mongo.Database
+	collection string
+}
+
+func NewUserRepository(db mongo.Database, collection string) model.UserRepository {
+	return &userRepository{
+		database:   db,
+		collection: collection,
+	}
+}
+
+func (ur *userRepository) Create(c context.Context, user *model.User) error {
+	collection := ur.database.Collection(ur.collection)
+
+	_, err := collection.InsertOne(c, user)
+
+	return err
+}
+
+func (ur *userRepository) GetByID(c context.Context, id string) (model.User, error) {
+	collection := ur.database.Collection(ur.collection)
+
+	var user model.User
+
+	idHex, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return user, err
+	}
+
+	err = collection.FindOne(c, bson.M{"_id": idHex}).Decode(&user)
+	return user, err
+}
