65aad6c40b1c8ef1de011ae850a21bf3a70141f9
Hasan A. Tekeoğlu <55619655+tekeoglan@users.noreply.github.com>
2023-09-20 14:11:31 +0300

    feat(): add login_route

---
+++ server/api/route/login_route.go
@@ -0,0 +1,27 @@
+package route
+
+import (
+	"github/tekeoglan/discord-clone/api/controller"
+	"github/tekeoglan/discord-clone/model"
+	"github/tekeoglan/discord-clone/mongo"
+	"github/tekeoglan/discord-clone/redis"
+	"github/tekeoglan/discord-clone/repository"
+	"github/tekeoglan/discord-clone/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+func NewLoginRoute(db mongo.Database,
+	cl redis.Client, group *gin.RouterGroup) {
+
+	ur := repository.NewUserRepository(db, model.CollectionUser)
+
+	cr := repository.NewCacheRepository(cl)
+
+	lr := controller.LoginController{
+		AccountService: service.NewAccountService(ur),
+		SessionService: service.NewSessionService(cr),
+	}
+
+	group.POST("/login", lr.Login)
+}
