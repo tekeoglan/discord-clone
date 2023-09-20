03c1e2f6cb6f92adbdcfd8cf05700e3f2bf291ec
Hasan A. Tekeoğlu <55619655+tekeoglan@users.noreply.github.com>
2023-09-20 14:10:13 +0300

    feat(): inject accountservice into register_route

--- server/api/route/register_route.go
+++ server/api/route/register_route.go
@@ -2,21 +2,22 @@ package route
 
 import (
 	"github/tekeoglan/discord-clone/api/controller"
-	"github/tekeoglan/discord-clone/bootstrap"
 	"github/tekeoglan/discord-clone/model"
 	"github/tekeoglan/discord-clone/mongo"
+	"github/tekeoglan/discord-clone/redis"
 	"github/tekeoglan/discord-clone/repository"
 	"github/tekeoglan/discord-clone/service"
-	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
-func NewRegisterRouter(env *bootstrap.Env, timeout time.Duration, db mongo.Database, group *gin.RouterGroup) {
+func NewRegisterRouter(db mongo.Database,
+	cl redis.Client, group *gin.RouterGroup) {
+
 	ur := repository.NewUserRepository(db, model.CollectionUser)
+
 	rc := controller.RegisterController{
-		RegisterService: service.NewRegisterService(ur),
-		Env:             env,
+		AccountService: service.NewAccountService(ur),
 	}
 
 	group.POST("/register", rc.Register)
