af4d8a38ce8d0db97eb0b52b7ff2baae900ebc99
Hasan A. Tekeoğlu <55619655+tekeoglan@users.noreply.github.com>
2023-09-20 14:11:55 +0300

    feat(): add auth middleware

---
+++ server/api/middleware/auth.go
@@ -0,0 +1,28 @@
+package middleware
+
+import (
+	"github/tekeoglan/discord-clone/model"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Auth(ss model.SessionService) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		sessionId, _ := c.Cookie("session_id")
+		if sessionId == "" {
+			c.JSON(http.StatusBadRequest, model.ErrorResponse{Message: "Session not provided."})
+			c.Abort()
+			return
+		}
+
+		val, _ := ss.RetriveSession(c, sessionId)
+		if val == "" {
+			c.JSON(http.StatusUnauthorized, model.ErrorResponse{Message: "Session invalid"})
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
+}
