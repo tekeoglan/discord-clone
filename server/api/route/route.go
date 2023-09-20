f95b1d217d754b03176380b38d0e53c0f6b74106
Hasan A. Tekeoğlu <55619655+tekeoglan@users.noreply.github.com>
2023-09-20 14:13:08 +0300

    feat(): setup new routes

--- server/api/route/route.go
+++ server/api/route/route.go
@@ -4,14 +4,26 @@ import (
 	"github/tekeoglan/discord-clone/api/middleware"
 	"github/tekeoglan/discord-clone/bootstrap"
 	"github/tekeoglan/discord-clone/mongo"
+	"github/tekeoglan/discord-clone/redis"
+	"github/tekeoglan/discord-clone/repository"
+	"github/tekeoglan/discord-clone/service"
 	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
-func Setup(env *bootstrap.Env, timeout time.Duration, db mongo.Database, gin *gin.Engine) {
+func Setup(env *bootstrap.Env, timeout time.Duration, db mongo.Database,
+	rc redis.Client, gin *gin.Engine) {
+
+	cr := repository.NewCacheRepository(rc)
+	ss := service.NewSessionService(cr)
+
 	publicRouter := gin.Group("")
 	publicRouter.Use(middleware.Timeout(timeout))
 
-	NewRegisterRouter(env, timeout, db, publicRouter)
+	userRouter := gin.Group("/channels")
+	userRouter.Use(middleware.Auth(ss))
+
+	NewRegisterRouter(db, rc, publicRouter)
+	NewLoginRoute(db, rc, publicRouter)
 }
