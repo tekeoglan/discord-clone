c8c8a5a96fd4e617b3041fb5fa939bc90c19e570
Hasan A. Tekeoğlu <55619655+tekeoglan@users.noreply.github.com>
2023-11-24 14:33:45 +0300

    config(): some changes

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/login", // Matched parameters can be used in the destination
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
