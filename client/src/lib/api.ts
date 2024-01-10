b3b5e431c87063a9dbe85fdf32d19413df477a88
Hasan A. Tekeoğlu <55619655+tekeoglan@users.noreply.github.com>
2024-01-10 18:48:15 +0300

    chor(): some changes

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,26 +1,30 @@
+const baseUrl = process.env.NEXT_PUBLIC_API_BASE_PATH;
+
+const wsUrl = baseUrl ? baseUrl.replace(/^http/, "ws") : "";
+
 export const endpoints = {
-  WebSocket: `${process.env.NEXT_PUBLIC_WS_PATH}`,
-  Login: `${process.env.NEXT_PUBLIC_API_BASE_PATH}/login`,
-  Logout: `${process.env.NEXT_PUBLIC_API_BASE_PATH}/logout`,
-  Register: `${process.env.NEXT_PUBLIC_API_BASE_PATH}/register`,
-  FetchUser: `${process.env.NEXT_PUBLIC_API_BASE_PATH}/fetchUser`,
-  GetFriendChannels: `${process.env.NEXT_PUBLIC_API_BASE_PATH}/channel/fc/user`,
+  WebSocket: `${wsUrl}/ws`,
+  Login: `${baseUrl}/login`,
+  Logout: `${baseUrl}/logout`,
+  Register: `${baseUrl}/register`,
+  FetchUser: `${baseUrl}/fetchUser`,
+  GetFriendChannels: `${baseUrl}/channel/fc/user`,
   GetConfirmedFriends: (cursorPos: number) =>
-    `${process.env.NEXT_PUBLIC_API_BASE_PATH}/friend/getConfirmed?cursorPos=${cursorPos}`,
+    `${baseUrl}/friend/getConfirmed?cursorPos=${cursorPos}`,
   GetPendingFriends: (cursorPos: number) =>
-    `${process.env.NEXT_PUBLIC_API_BASE_PATH}/friend/getPending?cursorPos=${cursorPos}`,
+    `${baseUrl}/friend/getPending?cursorPos=${cursorPos}`,
   GetChannelMessages: (channelId: string, cursorPos: number) =>
-    `${process.env.NEXT_PUBLIC_API_BASE_PATH}/message/channel?channelId=${channelId}&cursorPos=${cursorPos}`,
+    `${baseUrl}/message/channel?channelId=${channelId}&cursorPos=${cursorPos}`,
   GetFcByUserIds: (friendId: string) =>
-    `${process.env.NEXT_PUBLIC_API_BASE_PATH}/channel/fc/byUserIds?friendId=${friendId}`,
+    `${baseUrl}/channel/fc/byUserIds?friendId=${friendId}`,
   CreateFriendChannel: (friendId: string) =>
-    `${process.env.NEXT_PUBLIC_API_BASE_PATH}/channel/fc?friendId=${friendId}`,
-  AddFriend: `${process.env.NEXT_PUBLIC_API_BASE_PATH}/friend/add`,
+    `${baseUrl}/channel/fc?friendId=${friendId}`,
+  AddFriend: `${baseUrl}/friend/add`,
   AcceptFriendRequest: (friendId: string) =>
-    `${process.env.NEXT_PUBLIC_API_BASE_PATH}/friend/confirm/${friendId}`,
+    `${baseUrl}/friend/confirm/${friendId}`,
   DeclineFriendRequest: (friendId: string) =>
-    `${process.env.NEXT_PUBLIC_API_BASE_PATH}/friend/remove?id=${friendId}`,
+    `${baseUrl}/friend/remove?id=${friendId}`,
   RemoveFriend: (userId: string) =>
-    `${process.env.NEXT_PUBLIC_API_BASE_PATH}/friend/removeByUserIds?userId=${userId}`,
-  SendMessage: `${process.env.NEXT_PUBLIC_API_BASE_PATH}/message/add`,
+    `${baseUrl}/friend/removeByUserIds?userId=${userId}`,
+  SendMessage: `${baseUrl}/message/add`,
 };
